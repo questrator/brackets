f4b0abebd92fdcede00dc7eda24cb68bf0c1b43f
questrator <drumtrack@ya.ru>
2022-07-13 22:24:55 +0300

    upd: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2) return false;
+  let pars = bracketsConfig.map(e => e.join(""));
+  function rec(str, pars) {
+    let out = str;
+    for (let v of pars) {
+      if (str.includes(v)) out = str.replaceAll(v, "");
+    }
+    if (str.length === out.length) return out;
+    else return rec(out, pars);
+  }
+  return rec(str, pars).length === 0;
 }
